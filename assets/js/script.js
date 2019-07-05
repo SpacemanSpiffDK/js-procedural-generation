d3ab9649b44baccd3ac8270090c1ce782cb845ea
Dan Høegh <dan.hoegh@gmail.com>
2019-07-05 10:52:32 +0200

    Array done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,48 @@
 // JS by Dan Høegh
-// UCN MMD 2018
+// UCN MMD 2019
 
-window.onload = function(){
+let proceduralTable = [];
+let performanceText;
 
-	let a = 0;
-	let b = 1;
-	let c = 0;
-	let keys = 1000;
-	let startPoint = 1000;
-	let table = [];
+let js = {
+	procedural: {
+		init: function(keys){
+			let t0 = performance.now();
+			let a = 0;
+			let b = 1;
+			let c = 0;
+			let startPoint = 1000;
 
-
-	for (let i = 0; i < keys + startPoint; i++) {
-	  c = a + b;
-	  a = b;
-	  b = c;
-	  if (!(Number.isSafeInteger(b))) {
-	    b = Math.floor(b / i);
-	  }
-	  if (i > startPoint) {
-		table.push(getDigits(" " + b));
-	  }
+			for (let i = 0; i < keys + startPoint; i++) {
+				c = a + b;
+				a = b;
+				b = c;
+				if (!(Number.isSafeInteger(b))) {
+					b = Math.floor(b / i);
+				}
+				if (i > startPoint) {
+					proceduralTable.push(js.procedural.get3Digits(" " + b));
+				}
+			}
+			let t1 = performance.now();
+			document.querySelector("#performance").innerHTML = `Generation of ${keys} numbers took ${Math.round(t1 - t0)} milliseconds`;
+		},
+		get3Digits: function(str){
+			str = str.substr(str.length - 3, 3);
+			return str;			
+		},
+		get: function(i){
+			return proceduralTable[i];
+		},
+		generate: function(keys){
+			js.procedural.init(keys);
+			let str = "";
+			for (let i = 0; i < keys - 1; i++){
+				str += js.procedural.get(i) + " ";
+			}
+			document.querySelector('#output').innerHTML = str;
+		}
 	}
-
-	function getDigits(str) {
-	  str = str.substr(str.length - 3, 3);
-	  return str;
-	}
-
-	document.querySelector('.output').innerHTML = table;
 }
+
+
